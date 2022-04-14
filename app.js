dc9db1118af72be5dbc630d12951f04c46b0fef4
Daniel Mason <danmasson0@gmail.com>
2022-04-14 17:57:49 -0400

    created gsap animation, for home, messsages and bell svg to experiment with for use in app  production

---
+++ app.js
@@ -0,0 +1,58 @@
+// Home Svg
+
+const tl = gsap.timeline({
+  defaults: { duration: 0.35, ease: "Power2.easeOut" },
+});
+
+const home = document.querySelector(".home");
+const notification = document.querySelector(".notification");
+const messages = document.querySelector(".messages");
+
+gsap.set(".feather", { scale: 0, transformOrigin: "center" });
+home.addEventListener("click", () => {
+  gsap.fromTo(".home-svg", { scale: 1 }, { scale: 0.9, yoyo: true, repeat: 1 });
+  gsap.fromTo(
+    ".feather",
+    { y: -5, scale: 0 },
+    { y: 20, scale: 1.6, duration: 0.9, stagger: 0.25 }
+  );
+  gsap.fromTo(".right-feather", { x: 0 }, { x: 5 });
+});
+
+// Bell Svg
+// Notification animation, setting the transform origin
+gsap.set(".bell", { transformOrigin: "top center" });
+gsap.set(".ringer", { transformOrigin: "top center" });
+gsap.set(".wave", { opacity: 0, transformOrigin: "bottom" });
+
+notification.addEventListener("click", () => {
+  gsap.fromTo(
+    ".bell",
+    { rotation: -5 },
+    { rotation: 0, duration: 2, ease: "elastic.out(4, 0.2)" }
+  );
+  gsap.fromTo(
+    ".ringer",
+    { rotation: -3, x: 0.5 },
+    { rotation: 0, x: 0, duration: 1, ease: "elastic.out(5, 0.2)" }
+  );
+  gsap.fromTo(
+    ".wave",
+    { scale: 0, opacity: 1 },
+    { scale: 1.3, opacity: 0, duration: 1 }
+  );
+});
+
+// Message Svg
+gsap.set(".flap", { transformOrigin: "top" });
+messages.addEventListener("click", () => {
+  tl.fromTo(".messages-svg", { scale: 1 }, { scale: 0.9 });
+  tl.fromTo(".flap", { scale: 1 }, { scale: -1 }, "<50%");
+  tl.fromTo(".messages-svg", { scale: 0.9 }, { scale: 1 }, "<50%");
+  tl.fromTo(
+    ".note",
+    { y: 0, opacity: 1 },
+    { y: -40, opacity: 0, duration: 0.75 }
+  );
+  tl.to(".flap", { scale: 1 }, "<50%");
+});
